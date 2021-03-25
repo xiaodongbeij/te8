282e44cd14db8d334a492b8e1bc57be737195765
qingfeng <892608682@qq.com>
2021-03-25 17:42:22 +0800

    ces

--- app/appapi/controller/GameController.php
+++ app/appapi/controller/GameController.php
@@ -42,8 +42,8 @@ class GameController extends HomebaseController
 
 //        $this->start = date("Y-m-d H:i:s",strtotime('-20 minute'));
 //        $this->end = date("Y-m-d H:i:s");
-        $this->start = date("Y-m-d H:i:s",strtotime('-10 day'));
-        $this->end = date("Y-m-d H:i:s",strtotime('-8 day'));
+        $this->start = date("Y-m-d H:i:s",strtotime('-9 day'));
+        $this->end = date("Y-m-d H:i:s",strtotime('-7 day'));
         connectionRedis();
         $con = getConfigPri();
         $this->domain = $con['tripartite_domain'];
