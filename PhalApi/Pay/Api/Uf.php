3a6c4d5693a8f956c7fb8233da9893cb20b077a2
qingfeng <892608682@qq.com>
2021-03-29 21:22:51 +0800

    ces

--- PhalApi/Pay/Api/Uf.php
+++ PhalApi/Pay/Api/Uf.php
@@ -46,10 +46,13 @@ class Api_Uf extends PhalApi_Api {
 //        var_dump(json_encode($data));
 //        var_dump($info['action']);
 //        die;
-        $res = $this->curl->post($info['action'],$data);
-        var_dump($res);die;
+//        $res = $this->curl->post($info['action'],$data);
+//        var_dump($res);die;
 //        $res = curl($info['action'],$data,1);
 //        var_dump($res);die;
+        $res = buildRequestForm($info['action'], $data);
+        var_dump($res);
+        die;
 
 
         if (strpos($res, '订单创建成功') !== false) {
