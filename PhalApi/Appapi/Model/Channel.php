4230f8600b4fc1a64230a7db55ff204575a126db
qingfeng <892608682@qq.com>
2021-03-30 12:23:55 +0800

    ces

--- PhalApi/Appapi/Model/Channel.php
+++ PhalApi/Appapi/Model/Channel.php
@@ -7,7 +7,7 @@ class Model_Channel extends PhalApi_Model_NotORM
     public function get_channel($changeid) {
 
 //        $charge=DI()->notorm->charge_rules->select('*')->where('id=?',$changeid)->fetchOne();
-        $channel = DI()->notorm->channel->select('*')->where('id=?',$changeid)->fetchOne();
+        $channel = DI()->notorm->channel->select('*')->where('status=?',1)->where('id=?',$changeid)->fetchOne();
 
         if(!$channel){
             return 1003;
