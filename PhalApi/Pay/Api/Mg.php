09168e4f556250b749db8e6061cbd62d4fa9a56b
qingfeng <892608682@qq.com>
2021-04-05 22:58:58 +0800

    ces

--- PhalApi/Pay/Api/Mg.php
+++ PhalApi/Pay/Api/Mg.php
@@ -43,7 +43,7 @@ class Api_Mg extends PhalApi_Api {
 //        var_dump($data);
 //        var_dump($info['action']);die;
 //        $res = curl($info['action'],$data,1,1,true);
-        $res = $this->curl($info['action'],$data,0,1);
+        $res = $this->get($info['action'],$data);
         var_dump($res);
         die;
         $res = json_decode($res,true);
@@ -70,6 +70,29 @@ class Api_Mg extends PhalApi_Api {
 //        $str = substr($str, 0, -1);
         $str .= 'merchantKey=' . $key;
         return strtoupper(md5($str));
+    }
+
+    function get($url,$data){
+        $headers = array('Content-Type: application/x-www-form-urlencoded');
+        $curl = curl_init(); // 启动一个CURL会话
+        curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+        curl_setopt($curl, CURLOPT_POST, 0); // 发送一个常规的Post请求
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data)); // Post提交的数据包
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        $result = curl_exec($curl); // 执行操作
+        if (curl_errno($curl)) {
+            echo 'Errno'.curl_error($curl);//捕抓异常
+        }
+        curl_close($curl); // 关闭CURL会话
+        return $result;
     }
 
     //curl请求
