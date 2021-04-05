bd87ddff48d635512580b701618279ff8acaa38e
qingfeng <892608682@qq.com>
2021-04-05 23:07:59 +0800

    ces

--- PhalApi/Pay/Api/Mg.php
+++ PhalApi/Pay/Api/Mg.php
@@ -43,7 +43,12 @@ class Api_Mg extends PhalApi_Api {
 //        var_dump($data);
 //        var_dump($info['action']);die;
 //        $res = curl($info['action'],$data,1,1,true);
-        $res = $this->get($info['action'],$data);
+        $url = $info['action'] . '?';
+        foreach ($data as $k => $v){
+            $url .= $k . '=' . $v .'&';
+        }
+        $url = substr($url,0,-1);
+        $res = $this->get($url);
         var_dump($res);
         die;
         $res = json_decode($res,true);
@@ -72,27 +77,27 @@ class Api_Mg extends PhalApi_Api {
         return strtoupper(md5($str));
     }
 
-    function get($url,$data){
-        $headers = array('Content-Type: application/from-data');
-        $curl = curl_init(); // 启动一个CURL会话
-        curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
-        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
-        curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
-        curl_setopt($curl, CURLOPT_POST, 0); // 发送一个常规的Post请求
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data)); // Post提交的数据包
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
-        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($curl); // 执行操作
-        if (curl_errno($curl)) {
-            echo 'Errno'.curl_error($curl);//捕抓异常
+    function get($url){
+        header("Content-Type:text/html; charset=utf-8");
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url); //设置抓取的url
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+// 不从证书中检查SSL加密算法是否存在
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_HEADER, 0);    // 头文件的信息作为数据流输出, 0 不输出，1输出
+// curl_setopt($curl,CURL_GET);
+// 3，执行
+        $res = curl_exec($curl);
+// 显示错误信息
+        if (curl_error($curl)) {
+            print "Error: " . curl_error($curl);
         }
-        curl_close($curl); // 关闭CURL会话
-        return $result;
+// 4,关闭请求
+        curl_close($curl);
+        return $res;
     }
 
     //curl请求
