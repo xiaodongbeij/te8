36dae920ffff39cd7e3e97b482a54c04eee6d349
qingfeng <892608682@qq.com>
2021-04-05 23:20:42 +0800

    ces

--- PhalApi/Pay/Api/Mg.php
+++ PhalApi/Pay/Api/Mg.php
@@ -43,13 +43,9 @@ class Api_Mg extends PhalApi_Api {
 //        var_dump($data);
 //        var_dump($info['action']);die;
 //        $res = curl($info['action'],$data,1,1,true);
-        $url = $info['action'] . '?';
-        foreach ($data as $k => $v){
-            $url .= $k . '=' . $v .'&';
-        }
-        $url = substr($url,0,-1);
-        $res = $this->get($url);
-        var_dump($res);
+
+        $this->buildRequestForm($info['action'],$data,'GET');
+//        var_dump($res);
         die;
         $res = json_decode($res,true);
         if ($res['code'] == 200){
