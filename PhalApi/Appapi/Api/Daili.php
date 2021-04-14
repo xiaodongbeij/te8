529d0cb96625921ce2ea6c7d05b206caa8747ec5
qingfeng <892608682@qq.com>
2021-04-14 23:17:49 +0800

    ces

--- PhalApi/Appapi/Api/Daili.php
+++ PhalApi/Appapi/Api/Daili.php
@@ -320,7 +320,7 @@ class Api_Daili extends PhalApi_Api
 //            var_dump($where);die;
             $list = DI()->notorm->game_ticket
                 ->where("$where")
-                ->select("user_id,money,FROM_UNIXTIME(addtime, '%Y-%m-%d %H:%i:%s') addtime,prize")
+                ->select("user_id,money,FROM_UNIXTIME(addtime, '%Y-%m-%d %H:%i:%s') addtime,prize,show_name game_name")
                 ->order('id desc')
                 ->limit(($page-1) * $page_size,$page_size)
                 ->fetchAll();
