4a8d673f3e2e2dca8dd5d59c7268d37038d64062
qingfeng <892608682@qq.com>
2021-04-14 16:50:34 +0800

    cecs

--- PhalApi/Appapi/Api/Daili.php
+++ PhalApi/Appapi/Api/Daili.php
@@ -555,7 +555,7 @@ class Api_Daili extends PhalApi_Api
 //            ->order('id desc')
 //            ->limit(($page-1) * $page_size,$page_size)
 //            ->fetchAll();
-        $sql = "select uc.user_id,u.user_nicename,uc.change_money,uc.next_money,uc.status,FROM_UNIXTIME(uc.addtime, '%Y-%m-%d %H:%i:%s') addtime from cmf_user_change uc join cmf_user u on uc.user_id = u.id where $where order by uc.id desc limit $of,$page_size";
+        $sql = "select uc.user_id,u.user_nicename,uc.change_money,uc.next_money,uc.status,FROM_UNIXTIME(uc.addtime, '%Y-%m-%d %H:%i:%s') addtime from cmf_user_change uc join cmf_user u on uc.user_id = u.id where $where desc limit $of,$page_size";
         $list = DI()->notorm->user_change->queryAll($sql);
 
         foreach ($list as $k => $v){
