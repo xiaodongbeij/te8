fb3a6d7e31f1ed2186c4030e222b81f7246c9c08
qingfeng <892608682@qq.com>
2021-04-17 15:44:25 +0800

    ces

--- app/admin/controller/ManualController.php
+++ app/admin/controller/ManualController.php
@@ -154,7 +154,6 @@ class ManualController extends AdminbaseController {
                 //手动充值账变记录
                 $id = DB::name('charge_admin')->insertGetId($data);
                 $res = user_change_action($touid,$change_type,$coin,$data['remarks'],$id);
-                $id = 0;
                 if ($id && $res){
                     Db::commit();
                     if ($type == 1){
