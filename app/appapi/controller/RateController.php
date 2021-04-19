fb95ab62a5a33805b6a2ec730ad927f3a3bd41f2
shi_jun <1483259783@qq.com>
2021-04-19 23:05:10 +0800

    281

--- app/appapi/controller/RateController.php
+++ app/appapi/controller/RateController.php
@@ -552,46 +552,46 @@ class RateController extends HomebaseController
            'sign' => md5($str)
        ];
    }
-//
-//    protected function curl($url, $params = false, $ispost = 0, $https = 0)
-//    {
-//        $httpInfo = array();
-//        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-//        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
-//        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-//        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//        if ($https) {
-//            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
-//            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
-//        }
-//        if ($ispost) {
-//            curl_setopt($ch, CURLOPT_POST, true);
-//            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-//            curl_setopt($ch, CURLOPT_URL, $url);
-//        } else {
-//            if ($params) {
-//                if (is_array($params)) {
-//                    $params = http_build_query($params);
-//                }
-//                curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
-//            } else {
-//                curl_setopt($ch, CURLOPT_URL, $url);
-//            }
-//        }
-//
-//        $response = curl_exec($ch);
-//
-//        if ($response === FALSE) {
-//            //echo "cURL Error: " . curl_error($ch);
-//            return false;
-//        }
-//        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-//        $httpInfo = array_merge($httpInfo, curl_getinfo($ch));
-//        curl_close($ch);
-//        return $response;
-//    }
+   
+   protected function curl($url, $params = false, $ispost = 0, $https = 0)
+   {
+       $httpInfo = array();
+       $ch = curl_init();
+       curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+       curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
+       curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+       curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       if ($https) {
+           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
+           curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
+       }
+       if ($ispost) {
+           curl_setopt($ch, CURLOPT_POST, true);
+           curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+           curl_setopt($ch, CURLOPT_URL, $url);
+       } else {
+           if ($params) {
+               if (is_array($params)) {
+                   $params = http_build_query($params);
+               }
+               curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
+           } else {
+               curl_setopt($ch, CURLOPT_URL, $url);
+           }
+       }
+
+       $response = curl_exec($ch);
+
+       if ($response === FALSE) {
+           //echo "cURL Error: " . curl_error($ch);
+           return false;
+       }
+       $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+       $httpInfo = array_merge($httpInfo, curl_getinfo($ch));
+       curl_close($ch);
+       return $response;
+   }
     //user_rate1加数据
 //    public function insert()
 //    {
